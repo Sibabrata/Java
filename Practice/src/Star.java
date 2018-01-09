8cc3ad7b86cb588d08d7f9657f8e21a6b055e927
Choudhary Sibabrata Madan Roy <choudharysibabrata@QAIT.COM>
2018-01-09 14:18:20 +0530

    New 1

---
+++ Practice/src/Star.java
@@ -0,0 +1,18 @@
+
+public class Star {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+			
+			for(int j=5;i<=j;j--){
+			
+			System.out.print("*");
+			
+			}//end of inner loop	
+			
+			System.out.println(" ");
+		}//end of outer loop
+
+	}//end of main
+
+}//end of class
